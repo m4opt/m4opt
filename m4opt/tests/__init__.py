7a0f759b0c488561bf9ed6456d300e4cd0852120
Leo Singer <leo.singer@ligo.org>
2021-10-04 10:06:20 -0400

    Document how Gurobi license type check works

--- m4opt/tests/__init__.py
+++ m4opt/tests/__init__.py
@@ -3,7 +3,16 @@ import pytest
 
 
 def gurobi_problem_size_is_limited():
-    """Determine if the Gurobi problem size is limited by the license."""
+    """Determine if the Gurobi problem size is limited by the license.
+
+    Notes
+    -----
+    According to the `Gurobi EULA <https://www.gurobi.com/eula>`_, the free
+    evaluation license has a problem size limit of 2000 variables. This
+    function tests if a full license is present by attempting to solve a
+    trivial problem with just one too many variables for the free license.
+
+    """
     m = Model()
     m.addMVar(2001)
 
