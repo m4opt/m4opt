b7972d47e20f5c433e4f2877d9010341293b28a4
Bard <>
2022-02-17 17:52:54 -0500

    pep shenangians

--- m4opt/Objects/Bandpass.py
+++ m4opt/Objects/Bandpass.py
@@ -3,11 +3,12 @@ Bandpass Object: holds bandpass information
 
 """
 
-#dependencies
+# dependencies
 from astropy.modeling import Model
 
-# TODO: Should we merge this with PhotonSource? it has essentially the same features
-# except it uses Spectral Element instead of SourceSpectrum
+# TODO: Should we merge this with PhotonSource? it has essentially the same
+# features except it uses Spectral Element instead of SourceSpectrum
+
 
 class Bandpass(Model):
     """
@@ -16,19 +17,20 @@ class Bandpass(Model):
     Parameters
     ----------
     name : string
-        Name of object. Used for accessing internal parameters in compound background model.
-    bandpass : ``synphot.SpectralElement`` 
+        Name of object. Used for accessing internal parameters
+        in compound background model.
+    bandpass : ``synphot.SpectralElement``
     """
 
     # definitions required for astropy.Model
-    n_inputs = 1 # wavelength
-    n_outputs = 1 # scaling factor
+    n_inputs = 1  # wavelength
+    n_outputs = 1  # scaling factor
 
-    def __init__(self, name="bandpass", bandpass = None):
+    def __init__(self, name="bandpass", bandpass=None):
         self.bandpass = bandpass
         super().__init__()
 
-        #goes after __init__() because reasons(?)
+        # goes after __init__() because reasons(?)
         self.name = name
 
     def valid(self):
--- m4opt/Objects/Instrument.py
+++ m4opt/Objects/Instrument.py
@@ -5,27 +5,29 @@ contains information about instrument
 """
 
 from astropy import units as u
+import numpy as np
 
 # default instrument readouts:
 dorado = {
-    "area" : 100.0 * u.cm**2,
-    "plate_scale" : 25.0 * u.arcsec * u.pix**-1,
-    "npix" : np.pi * 0.89**2,
-    "aperture_correction" : 0.7,
-    "dark_noise" : 4. * 30. * 0.124 * u.hour**-1,
-    "read_noise" : 5.0
+    "area": 100.0 * u.cm**2,
+    "plate_scale": 25.0 * u.arcsec * u.pix**-1,
+    "npix": np.pi * 0.89**2,
+    "aperture_correction": 0.7,
+    "dark_noise": 4. * 30. * 0.124 * u.hour**-1,
+    "read_noise": 5.0
 }
 
+
 class Instrument:
-    def __init__(self, area=None, plate_scale=None, npix=None, aperture_correction=None,
-                    dark_noise = None, read_noise=None):
-        
+    def __init__(self, area=None, plate_scale=None, npix=None,
+                 aperture_correction=None, dark_noise=None, read_noise=None):
+
         self.area = area
         self.plate_scale = plate_scale
         self.npix = npix
         self.aperture_correction = aperture_correction
         self.dark_noise = dark_noise
-        self.read_noise = read_noise        
+        self.read_noise = read_noise
 
     @classmethod
     def dorado(cls):
