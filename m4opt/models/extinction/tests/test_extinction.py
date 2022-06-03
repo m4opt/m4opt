236947be60953ce2890c9177b09de97a9cf366db
Chris Bard <>
2022-06-03 16:57:01 -0400

    first attempt at unit tests

---
+++ m4opt/models/extinction/tests/test_extinction.py
@@ -0,0 +1,53 @@
+from ..airmass import Airmass, AtmoExtinction
+from ..airmass import simple_airmass, KastenYoung_airmass
+
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+from astropy.time import Time
+
+
+AIRMASS_KP = Airmass.kpno()
+TARGET = SkyCoord.from_name('m33')
+TIME = Time('2012-7-13 07:00:00')
+
+
+def test_simple():
+    assert simple_airmass(0*u.deg).value == 1.
+    assert simple_airmass(0.785398*u.rad).value == 1.4142133312942642
+    assert simple_airmass(89*u.deg).value == 57.2986884985499
+
+
+def test_kasyoung():
+    assert KastenYoung_airmass(0.*u.rad).value == 0.9997119918558381
+    assert KastenYoung_airmass(45.*u.deg).value == 1.4125952520262743
+    assert simple_airmass(89*u.deg).value == 26.31055506838526
+
+
+def test_Atmo_KPNO_linear():
+    atmo = AtmoExtinction.at(AIRMASS_KP, TARGET, TIME, table_name='kpno',
+                             table_method='linear')
+    assert atmo(3250*u.Angstrom).value == 0.006363833100109559
+    assert atmo(3275*u.Angstrom).value == 0.008351825903814643
+
+
+# default is kpno
+def test_Atmo_default_linear():
+    atmo = AtmoExtinction.at(AIRMASS_KP, TARGET, TIME, table_method='linear')
+    assert atmo(3250*u.Angstrom).value == 0.006363833100109559
+    assert atmo(3275*u.Angstrom).value == 0.008351825903814643
+
+
+def test_Atmo_default_nearest():
+    atmo = AtmoExtinction.at(AIRMASS_KP, TARGET, TIME, table_name='kpno',
+                             table_method='nearest')
+    assert atmo(3250*u.Angstrom).value == 0.006363833100109559
+    assert atmo(3275*u.Angstrom).value == 0.010915792117502507
+
+
+"""
+@pytest.mark.remote_data
+def test_read_extinction():
+    freq, ext = read_extinction_table("parnal")
+    assert freq[20].value == 422242898591549.25
+    assert ext[20] == 0.072
+"""
