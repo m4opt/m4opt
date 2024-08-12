72f659b8c7df7282bc16696f281276b06325858e
Leo Singer <leo.singer@ligo.org>
2024-08-12 08:40:13 -0400

    Add sun separation constraint

--- m4opt/constraints/__init__.py
+++ m4opt/constraints/__init__.py
@@ -1,4 +1,5 @@
 from .core import Constraint
 from .earth_limb import EarthLimbConstraint
+from .sun_separation import SunSeparationConstraint
 
-__all__ = ("Constraint", "EarthLimbConstraint")
+__all__ = ("Constraint", "EarthLimbConstraint", "SunSeparationConstraint")
