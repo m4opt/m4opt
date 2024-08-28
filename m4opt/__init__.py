6aca8feabf38341029755bd33474300e7c04eb17
Leo Singer <leo.singer@ligo.org>
2024-08-28 02:00:24 -0400

    Add docs for slew speed functions

--- m4opt/__init__.py
+++ m4opt/__init__.py
@@ -1,4 +1,5 @@
 from ._astropy_init import *  # noqa
 from ._roll import nominal_roll
+from ._slew import slew_separation, slew_time
 
-__all__ = ("nominal_roll",)
+__all__ = ("nominal_roll", "slew_separation", "slew_time")
