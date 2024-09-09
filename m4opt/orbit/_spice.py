6a2c4819676c8a1cd09d825eb00aa8a8ecf31fa6
Leo Singer <leo.singer@ligo.org>
2024-09-08 21:52:56 -0400

    Teach Spice orbit class to correctly handle scalars

--- m4opt/orbit/_spice.py
+++ m4opt/orbit/_spice.py
@@ -41,12 +41,17 @@ class Spice(Orbit):
     ...     'https://archive.stsci.edu/missions/tess/models/TESS_EPH_PRE_LONG_2021252_21.bsp',
     ...     'https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_latest_high_prec.bpc',
     ...     'https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00010.tpc')
-    >>> orbit(Time('2021-10-31 00:00') + np.arange(3) * u.hour)
+    >>> t0 = Time('2021-10-31 00:00')
+    >>> orbit(t0)
+    <SkyCoord (ITRS: obstime=2021-10-31 00:00:00.000, location=(0.0, 0.0, 0.0) km): (x, y, z) in km
+        (259589.01504305, 267775.69181568, -6003.44398346)>
+    >>> orbit(t0 + np.arange(4) * u.hour)
     <SkyCoord (ITRS: obstime=['2021-10-31 00:00:00.000' '2021-10-31 01:00:00.000'
-     '2021-10-31 02:00:00.000'], location=(0., 0., 0.) km): (x, y, z) in km
+     '2021-10-31 02:00:00.000' '2021-10-31 03:00:00.000'], location=(0.0, 0.0, 0.0) km): (x, y, z) in km
         [(259589.01504305, 267775.69181568, -6003.44398346),
          (319237.52597807, 193229.46894121, -5243.0107051 ),
-         (358011.10059696, 105924.84848415, -4482.00355537)]>
+         (358011.10059696, 105924.84848415, -4482.00355537),
+         (373338.18149427,  11586.48011847, -3720.50003742)]>
 
     """  # noqa: E501
 
@@ -60,4 +65,4 @@ class Spice(Orbit):
     def __call__(self, time):
         et = _time_to_et(time)
         pos, _ = _spkgps(self._target, et, "IAU_EARTH", self._body)
-        return SkyCoord(pos, unit=u.km, frame=ITRS(obstime=time))
+        return SkyCoord(*pos.T, unit=u.km, frame=ITRS(obstime=time))
