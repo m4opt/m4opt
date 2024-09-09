6a9433f865583e3092d0872ca67b0329ff884abe
Leo Singer <leo.singer@ligo.org>
2024-09-09 09:14:42 -0400

    Compute count rate, not effective stimulus

--- m4opt/models/__init__.py
+++ m4opt/models/__init__.py
@@ -1,11 +1,11 @@
 from ._bandpass import Bandpass
 from ._extinction import DustExtinction
 from ._extrinsic import observing
-from ._math import effstim
+from ._math import countrate
 
 __all__ = (
     "Bandpass",
     "DustExtinction",
-    "effstim",
+    "countrate",
     "observing",
 )
--- m4opt/models/_math.py
+++ m4opt/models/_math.py
@@ -3,9 +3,10 @@ from operator import mul
 
 import numpy as np
 import sympy
+from astropy import units as u
 from astropy.modeling import CompoundModel, Model
 from scipy.interpolate import interp1d
-from synphot import Observation, SourceSpectrum
+from synphot import Observation, SourceSpectrum, SpectralElement
 
 from ._extinction import DustExtinction, DustExtinctionForSkyCoord, dust_map
 from ._extrinsic import ExtrinsicScaleFactor, state
@@ -20,45 +21,41 @@ class ModelSymbol(sympy.Dummy):
         return obj
 
 
-def effstim(
-    spectrum, bandpass, flux_unit=None, wavelengths=None, area=None, vegaspec=None
-):
+def countrate(
+    spectrum: SourceSpectrum, bandpass: SpectralElement
+) -> u.Quantity[u.count / (u.s * u.cm**2)]:
     """
     >>> from astropy.coordinates import EarthLocation, SkyCoord
     >>> from astropy.time import Time
     >>> from m4opt.models.background import ZodiacalBackground
     >>> from m4opt.models import DustExtinction
     >>> from m4opt.models import observing
-    >>> from m4opt.models._math import effstim
+    >>> from m4opt.models._math import countrate
     >>> import numpy as np
     >>> import synphot
     >>> from astropy import units as u
     >>> spectrum = synphot.SourceSpectrum(synphot.BlackBody1D, temperature=1000 * u.Kelvin) * DustExtinction()
     >>> band = synphot.SpectralElement.from_filter('johnson_r')
     >>> with observing(EarthLocation.of_site('Palomar'), SkyCoord(0 * u.deg, 0 * u.deg), Time('2024-01-01')):
-    ...     (spectrum * band)(2000 * u.angstrom)
-    <Quantity 6.25485842e-18 PHOTLAM>
+    ...     (spectrum * band)(6000 * u.angstrom)
+    <Quantity 1.12557248e+09 PHOTLAM>
     >>> with observing(EarthLocation.of_site('Palomar'), SkyCoord(*np.meshgrid(np.linspace(0, 360, 100), np.linspace(-90, 90, 200)), unit=u.deg), Time('2024-01-01')):
-    ...     effstim(spectrum, band)
-    <Quantity [[5.56548638e+10, 5.56548638e+10, 5.56548638e+10, ...,
-                5.56548638e+10, 5.56548638e+10, 5.56548638e+10],
-               [6.02847758e+10, 6.07662430e+10, 6.13964602e+10, ...,
-                6.00166956e+10, 6.01319796e+10, 6.02847758e+10],
-               [6.37149606e+10, 6.36491559e+10, 6.34114654e+10, ...,
-                6.16670282e+10, 6.28588841e+10, 6.37149606e+10],
+    ...     countrate(spectrum, band)
+    <Quantity [[1.16887524e+14, 1.16887524e+14, 1.16887524e+14, ...,
+                1.16887524e+14, 1.16887524e+14, 1.16887524e+14],
+               [1.26595837e+14, 1.27605355e+14, 1.28926750e+14, ...,
+                1.26033735e+14, 1.26275459e+14, 1.26595837e+14],
+               [1.33787878e+14, 1.33649910e+14, 1.33151561e+14, ...,
+                1.29494053e+14, 1.31992994e+14, 1.33787878e+14],
                ...,
-               [4.77455317e+10, 4.21534149e+10, 4.54415136e+10, ...,
-                5.44657755e+10, 5.28081993e+10, 4.77455317e+10],
-               [5.86405552e+10, 5.76323220e+10, 5.64942641e+10, ...,
-                5.64455489e+10, 5.77565225e+10, 5.86405552e+10],
-               [5.06104299e+10, 5.06104299e+10, 5.06104299e+10, ...,
-                5.06104299e+10, 5.06104299e+10, 5.06104299e+10]] PHOTLAM>
+               [1.00300151e+14, 8.85700541e+13, 9.54674777e+13, ...,
+                1.14393997e+14, 1.10917927e+14, 1.00300151e+14],
+               [1.23148237e+14, 1.21034114e+14, 1.18647710e+14, ...,
+                1.18545557e+14, 1.21294547e+14, 1.23148237e+14],
+               [1.06308797e+14, 1.06308797e+14, 1.06308797e+14, ...,
+                1.06308797e+14, 1.06308797e+14, 1.06308797e+14]] ct / (s cm2)>
     """
-
-    if flux_unit is None:
-        flux_unit = Observation._internal_flux_unit
-    flux_unit = Observation._validate_flux_unit(flux_unit)
-
+    count_rate_unit = u.count / (u.s * u.cm**2)
     extrinsic_scale_factors = []
     dust_extinction = None
 
@@ -91,47 +88,49 @@ def effstim(
                     f"Don't know how to evaluate coefficient symbol: {coef}"
                 )
 
-    def base_effstim_no_extinction(spectrum):
-        return Observation(spectrum, bandpass).effstim(
-            flux_unit=flux_unit,
-            wavelengths=bandpass.waveset,
-            area=area,
-            vegaspec=vegaspec,
+    def base_countrate_no_extinction(spectrum):
+        area = 1 * u.cm**2
+        return (
+            Observation(spectrum, bandpass).countrate(
+                wavelengths=bandpass.waveset,
+                area=area,
+            )
+            / area
         )
 
-    def base_effstim(spectrum):
+    def base_countrate(spectrum):
         @np.vectorize
-        def base_effstim_extinction_for_Ebv(Ebv):
-            return base_effstim_no_extinction(spectrum * DustExtinction(Ebv)).to_value(
-                flux_unit
-            )
+        def base_countrate_extinction_for_Ebv(Ebv):
+            return base_countrate_no_extinction(
+                spectrum * DustExtinction(Ebv)
+            ).to_value(count_rate_unit)
 
         if dust_extinction is not None:
             xp = dust_map().query(state.get().target_coord)
             n_samples = 512
             if np.size(xp) >= n_samples:
                 x = np.linspace(0, dust_extinction.model.Ebv_max, n_samples)
-                y = base_effstim_extinction_for_Ebv(x)
+                y = base_countrate_extinction_for_Ebv(x)
                 xp = dust_map().query(state.get().target_coord)
                 return (
                     interp1d(x, y, kind="cubic", copy=False, assume_sorted=True)(xp)
-                    * flux_unit
+                    * count_rate_unit
                 )
             else:
-                return base_effstim_extinction_for_Ebv(xp) * flux_unit
+                return base_countrate_extinction_for_Ebv(xp) * count_rate_unit
         else:
-            return base_effstim_no_extinction(spectrum)
+            return base_countrate_no_extinction(spectrum)
 
     def evaluate_term(term):
         match term:
             case sympy.Add():
                 return sum(evaluate_term(arg) for arg in term.args)
             case sympy.Mul():
-                return base_effstim(
+                return base_countrate(
                     SourceSpectrum(reduce(mul, (arg.model for arg in term.args)))
                 )
             case sympy.Symbol():
-                return base_effstim(SourceSpectrum(term.model))
+                return base_countrate(SourceSpectrum(term.model))
             case _:
                 raise NotImplementedError(
                     f"Don't know how to evaluate term symbol: {term}"
