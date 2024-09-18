1fac6f8bab7d671fa86298d006cebb5456c95844
Leo Singer <leo.singer@ligo.org>
2024-09-18 14:23:56 -0400

    Add docstring

--- m4opt/constraints/__init__.py
+++ m4opt/constraints/__init__.py
@@ -1,3 +1,13 @@
+"""Field of regard constraints.
+
+The field of regard is the region of the sky that a detector is allowed to
+point at. These classes model various constraints on the field of regard, as
+functions of the location of the detector in space (an
+:class:`~astropy.coordinates.EarthLocation` instance), the target at which the
+detector is pointed (a :class:`~astropy.coordinates.SkyCoord` instance), and
+the time of the observation (a :class:`~astropy.time.Time` instance).
+"""
+
 from ._body_separation import MoonSeparationConstraint, SunSeparationConstraint
 from ._core import Constraint
 from ._earth_limb import EarthLimbConstraint
