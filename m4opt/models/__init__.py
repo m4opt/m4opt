2845f55f1ccfafb25b13feb8143679e0422d1c27
Leo Singer <leo.singer@ligo.org>
2024-09-19 17:38:25 -0400

    Remove bandpass module

--- m4opt/models/__init__.py
+++ m4opt/models/__init__.py
@@ -1,10 +1,8 @@
-from ._bandpass import Bandpass
 from ._detector import Detector
 from ._extinction import DustExtinction
 from ._extrinsic import observing
 
 __all__ = (
-    "Bandpass",
     "Detector",
     "DustExtinction",
     "observing",
