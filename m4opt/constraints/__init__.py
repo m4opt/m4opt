04d4edfc0c8cf72dce6882af7dfb5ac4e7b44550
Leo Singer <leo.singer@ligo.org>
2024-09-18 23:22:55 -0400

    Add Galactic latitude constraint

--- m4opt/constraints/__init__.py
+++ m4opt/constraints/__init__.py
@@ -1,10 +1,12 @@
 from ._body_separation import MoonSeparationConstraint, SunSeparationConstraint
 from ._core import Constraint
 from ._earth_limb import EarthLimbConstraint
+from ._galactic import GalacticLatitudeConstraint
 
 __all__ = (
     "Constraint",
     "EarthLimbConstraint",
+    "GalacticLatitudeConstraint",
     "MoonSeparationConstraint",
     "SunSeparationConstraint",
 )
