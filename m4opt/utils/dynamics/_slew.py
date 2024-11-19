bf05a67b1251b26c45ffdc8a49fe64f0f8700709
Leo Singer <leo.singer@ligo.org>
2024-11-19 11:22:14 -0500

    Add settling time, plot of optimal slew

--- m4opt/utils/dynamics/_slew.py
+++ m4opt/utils/dynamics/_slew.py
@@ -14,9 +14,59 @@ def matrix_trace(matrix):
 class Slew:
     """Model the slew time for a spacecraft.
 
+    Notes
+    -----
     The optimal slew consists of an acceleration phase at the maximum
     acceleration, possibly a coasting phase at the maximum angular velocity,
-    and a deceleration phase at the maximum acceleration.
+    and a deceleration phase at the maximum acceleration, as shown in the
+    figure below.
+
+    .. plot::
+        :include-source: False
+        :show-source-link: False
+
+        from matplotlib import pyplot as plt
+        import numpy as np
+
+        def cases(*args):
+            *args, else_value = args
+            while args:
+                *args, cond, if_value = args
+                else_value = np.where(cond, if_value, else_value)
+            return else_value
+
+
+        fig_width, fig_height = plt.rcParams['figure.figsize']
+        scale = 0.5
+        fig, axs = plt.subplots(
+            3, 2, sharex=True, sharey='row',
+            figsize=(2 * scale * fig_width, 3 * scale * fig_height),
+            tight_layout=True)
+
+        t = np.linspace(0, 0.6, 1000)
+        axs[0, 0].plot(t, cases(t <= 0.2, 1, t <= 0.4, -1, 0))
+        axs[1, 0].plot(t, cases(t <= 0.2, t, t <= 0.4, 0.4 - t, 0))
+        axs[2, 0].plot(t, cases(t <= 0.2, 0.5 * t**2, t <= 0.4, 0.02 + 0.2 * (t - 0.2) - 0.5 * (t - 0.2)**2, 0.04))
+        t = np.linspace(0, 1, 1000)
+        axs[1, 1].plot(t, cases(t <= 0.3, t, t <= 0.5, 0.3, t <= 0.8, 0.8 - t, 0))
+        axs[0, 1].plot(t, cases(t <= 0.3, 1, t <= 0.5, 0, t <= 0.8, -1, 0))
+        axs[2, 1].plot(t, cases(t <= 0.3, 0.5 * t**2, t <= 0.5, 0.045 + 0.3 * (t - 0.3), t <= 0.8, 0.105 + 0.3 * (t - 0.5) - 0.5 * (t - 0.5)**2, 0.15))
+        axs[2, 0].set_xlabel('time')
+        axs[2, 1].set_xlabel('time')
+        axs[0, 0].set_ylabel('acceleration')
+        axs[1, 0].set_ylabel('velocity')
+        axs[2, 0].set_ylabel('position')
+        axs[0, 0].set_title('short slew')
+        axs[0, 1].set_title('long slew')
+
+        axs[0, 0].set_yticks([-1, 0, 1])
+        axs[0, 0].set_yticklabels(['-|max|', '0', '+|max|'])
+        axs[1, 0].set_yticks([0, 0.3])
+        axs[1, 0].set_yticklabels(['0', 'max'])
+        axs[2, 0].set_yticks([0])
+        axs[2, 0].set_yticklabels(['0'])
+        axs[2, 0].set_xticks([])
+        axs[2, 1].set_xticks([])
     """
 
     max_angular_velocity: u.Quantity[u.physical.angular_velocity]
@@ -25,14 +75,18 @@ class Slew:
     max_angular_acceleration: u.Quantity[u.physical.angular_acceleration]
     """Maximum angular acceleration."""
 
+    settling_time: u.Quantity[u.physical.time] = 0 * u.second
+    """Time to settle to rest after a slew."""
+
     @staticmethod
     def _time(
         x: u.Quantity[u.physical.angle],
         v: u.Quantity[u.physical.angular_velocity],
         a: u.Quantity[u.physical.angular_acceleration],
+        s: u.Quantity[u.physical.time],
     ) -> u.Quantity[u.physical.time]:
         xc = np.square(v) / a
-        return np.where(x <= xc, np.sqrt(4 * x / a), (x + xc) / v)
+        return np.where(x <= xc, np.sqrt(4 * x / a), (x + xc) / v) + s
 
     def time(
         self,
@@ -46,6 +100,7 @@ class Slew:
             self.separation(center1, center2, roll1, roll2),
             self.max_angular_velocity,
             self.max_angular_acceleration,
+            self.settling_time,
         )
 
     @staticmethod
