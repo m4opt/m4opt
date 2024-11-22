148a22b74457f2a02361df78a91306acb4c5d67a
Leo Singer <leo.singer@ligo.org>
2024-11-22 08:36:03 -0500

    Update to astropy 7.0.0

--- m4opt/utils/numpy.py
+++ m4opt/utils/numpy.py
@@ -1,31 +1,6 @@
 """Utilities for working with Numpy arrays."""
 
 import numpy as np
-from astropy import units as u
-
-
-def arange_with_units(*args: u.Quantity) -> u.Quantity:
-    """A version of :obj:`numpy.arange` that works with Astropy units.
-
-    Examples
-    --------
-    >>> from astropy import units as u
-    >>> from m4opt.utils.numpy import arange_with_units
-    >>> arange_with_units(0 * u.m, 10 * u.m)
-    <Quantity [0., 1., 2., 3., 4., 5., 6., 7., 8., 9.] m>
-
-    Notes
-    -----
-    :obj:`numpy.arange` is known not to work with Astropy units. See:
-
-    - https://docs.astropy.org/en/stable/known_issues.html#numpy-array-creation-functions-cannot-be-used-to-initialize-quantity
-    - https://github.com/astropy/astropy/issues/17001
-    - https://github.com/astropy/astropy/pull/17120
-    """
-    # FIXME: Remove this when we depend upon astropy >= 7.0.0.
-    first, *rest = args
-    unit = first.unit
-    return np.arange(first.value, *(arg.to_value(unit) for arg in rest)) * unit
 
 
 def atmost_1d(a):
