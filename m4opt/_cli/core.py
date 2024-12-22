dd5a70573960a1371098eeb3d7e5b67d75a73b16
Leo Singer <leo.singer@ligo.org>
2024-12-22 09:06:39 -0500

    Monkeypatch typer to support Quantity parameters

--- m4opt/_cli/core.py
+++ m4opt/_cli/core.py
@@ -2,6 +2,9 @@ from typing import Annotated
 
 import click
 import typer
+from astropy import units as u
+from typer.main import get_click_type as _get_click_type
+from typer.main import lenient_issubclass
 
 from .. import __version__, missions
 
@@ -12,6 +15,34 @@ def version_callback(value: bool):
     if value:
         print(__version__)
         raise typer.Exit()
+
+
+class QuantityClickType(click.ParamType):
+    name = "Astropy quantity"
+
+    def convert(self, value, param, ctx):
+        result = u.Quantity(value)
+        if param is not None and (default := param.default) is not None:
+            target_physical_type = u.get_physical_type(u.Quantity(default))
+            physical_type = u.get_physical_type(result)
+            if physical_type != target_physical_type:
+                self.fail(
+                    f"value '{value}' cannot be converted to {target_physical_type}",
+                    param,
+                    ctx,
+                )
+        return result
+
+
+def get_click_type(*, annotation, parameter_info):
+    """Monkeypatch for typer.main.get_click_type to add support for new types."""
+    if lenient_issubclass(annotation, u.Quantity):
+        return QuantityClickType()
+    else:
+        return _get_click_type(annotation=annotation, parameter_info=parameter_info)
+
+
+typer.main.get_click_type = get_click_type
 
 
 @app.callback()
