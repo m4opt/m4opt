3fb0be57ff5b2d06059cf155c72b1fccfb539245
Leo Singer <leo.singer@ligo.org>
2024-12-22 10:35:01 -0500

    Test parsed quantity value

--- m4opt/_cli/tests/test_core.py
+++ m4opt/_cli/tests/test_core.py
@@ -4,23 +4,33 @@ from typer.testing import CliRunner
 
 from .. import core  # noqa: F401
 
+runner = CliRunner()
+
 
 def test_quantity():
     """Test CLI with quantity arguments."""
-    app = Typer()
-    runner = CliRunner()
 
-    @app.command()
-    def main(foo: u.Quantity = "100 s"):
-        assert foo == 100 * u.s
+    def run(*args):
+        app = Typer()
+        value = None
 
-    result = runner.invoke(app)
+        @app.command()
+        def main(foo: u.Quantity = "100 s"):
+            nonlocal value
+            value = foo
+
+        result = runner.invoke(app, args)
+        return result, value
+
+    result, value = run()
     assert result.exit_code == 0
+    assert value == 100 * u.s
 
-    result = runner.invoke(app, ["--foo=100s"])
+    result, value = run("--foo=200s")
     assert result.exit_code == 0
+    assert value == 200 * u.s
 
-    result = runner.invoke(app, ["--foo=100meter"])
+    result, value = run("--foo=100meter")
     assert result.exit_code != 0
     assert (
         "Invalid value for '--foo': value '100meter' cannot be converted to time"
