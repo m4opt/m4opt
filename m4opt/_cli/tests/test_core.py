f74fcaa04d7051df43e78becf3a3a78f9bed3da8
Leo Singer <leo.singer@ligo.org>
2024-12-22 10:35:16 -0500

    Refactor mission option

--- m4opt/_cli/tests/test_core.py
+++ m4opt/_cli/tests/test_core.py
@@ -2,6 +2,7 @@ from astropy import units as u
 from typer import Typer
 from typer.testing import CliRunner
 
+from ... import missions
 from .. import core  # noqa: F401
 
 runner = CliRunner()
@@ -36,3 +37,31 @@ def test_quantity():
         "Invalid value for '--foo': value '100meter' cannot be converted to time"
         in result.output
     )
+
+
+def test_mission():
+    """Test CLI with mission arguments."""
+
+    def run(*args):
+        app = Typer()
+        value = None
+
+        @app.command()
+        def main(foo: missions.Mission = missions.uvex):
+            nonlocal value
+            value = foo
+
+        result = runner.invoke(app, args)
+        return result, value
+
+    result, value = run()
+    assert result.exit_code == 0
+    assert value == missions.uvex
+
+    result, value = run("--foo=ultrasat")
+    assert result.exit_code == 0
+    assert value == missions.ultrasat
+
+    result, value = run("--foo=bar")
+    assert result.exit_code != 0
+    assert "Invalid value for '--foo': 'bar' is not one of" in result.output
