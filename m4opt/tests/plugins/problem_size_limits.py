d60a4302eeb774b03a3da3d797be4c51ec44ab03
Leo Singer <leo.singer@ligo.org>
2024-12-22 20:27:40 -0500

    Remove unused gurobipy code

--- m4opt/tests/plugins/problem_size_limits.py
+++ m4opt/tests/plugins/problem_size_limits.py
@@ -1,15 +1,10 @@
 import pytest
 from docplex.mp.utils import DOcplexLimitsExceeded
-from gurobipy import GRB, GurobiError
 
 
 def pytest_runtest_call(item):
     """Skip tests that exceed the Gurobi or CPLEX problem size."""
     try:
         item.runtest()
-    except GurobiError as e:
-        if e.errno == GRB.Error.SIZE_LIMIT_EXCEEDED:
-            pytest.skip("requires full version of Gurobi")
-        raise
     except DOcplexLimitsExceeded:
         pytest.skip("requires full version of CPLEX")
