40cbdac31c2015d642fbdb0f245790fd6f156cb8
Leo Singer <leo.singer@ligo.org>
2024-12-22 23:16:26 -0500

    Add end-to-end test of solution to optimality

--- m4opt/_cli/tests/conftest.py
+++ m4opt/_cli/tests/conftest.py
@@ -7,7 +7,7 @@ def run_cli():
     runner = CliRunner()
 
     def func(app, *args):
-        result = runner.invoke(app, args, catch_exceptions=False)
+        result = runner.invoke(app, [str(arg) for arg in args], catch_exceptions=False)
         print(result.output)
         return result
 
