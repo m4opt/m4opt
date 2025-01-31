8c7c443a71f164f6993694cde13d8d3dedab0c62
Leo Singer <leo.singer@ligo.org>
2025-01-30 23:55:28 -0500

    Add function to determine memory usage

--- docs/guide/utils.rst
+++ docs/guide/utils.rst
@@ -4,3 +4,4 @@ Utilities (`m4opt.utils`)
 
 .. automodapi:: m4opt.utils.console
 .. automodapi:: m4opt.utils.numpy
+.. automodapi:: m4opt.utils.resource
---
+++ m4opt/utils/resource.py
@@ -0,0 +1,37 @@
+"""Functions for monitoring resource usage."""
+
+from functools import cache
+from resource import RUSAGE_SELF, getrusage
+
+import numpy as np
+
+
+def _get_maxrss_raw():
+    return getrusage(RUSAGE_SELF).ru_maxrss
+
+
+def _get_bytes_per_maxrss_unit_func():
+    """Measure the units in bytes of maxrss.
+
+    The units are different on macOS/BSD and Linux. See
+    https://github.com/python/cpython/issues/64667.
+    """
+    maxrss1 = _get_maxrss_raw()
+    dummy = np.ones(1_000_000)
+    maxrss2 = _get_maxrss_raw()
+    return 1 << (
+        10 * int(np.rint(np.log(dummy.nbytes / (maxrss2 - maxrss1)) / np.log(1024)))
+    )
+
+
+@cache
+def _get_bytes_per_maxrss_unit():
+    import multiprocessing
+
+    with multiprocessing.Pool(1) as pool:
+        return pool.apply(_get_bytes_per_maxrss_unit_func)
+
+
+def get_maxrss_bytes():
+    """Get the memory usage of the current process and all of its children."""
+    return _get_maxrss_raw() * _get_bytes_per_maxrss_unit()
