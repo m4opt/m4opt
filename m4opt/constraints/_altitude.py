8268cf6686aca0070709441a0b1373533c1749c0
Ramodgwendé Weizmann Kiendrébéogo <kiend.weizman7@gmail.com>
2025-02-27 04:26:32 +0100

    Removed max_best_rescale method

--- m4opt/constraints/_altitude.py
+++ m4opt/constraints/_altitude.py
@@ -17,9 +17,6 @@ class AltitudeConstraint(Constraint):
         Minimum altitude of the target (inclusive). Default is `-90 deg`.
     max : `~astropy.units.Quantity`, optional
         Maximum altitude of the target (inclusive). Default is `90 deg`.
-    boolean_constraint : bool, optional
-        If True, the constraint returns a boolean array.
-        If False, it returns a float array scaled between 0 and 1.
 
     Examples
     --------
@@ -39,11 +36,9 @@ class AltitudeConstraint(Constraint):
         self,
         min: Optional[u.Quantity] = -90 * u.deg,
         max: Optional[u.Quantity] = 90 * u.deg,
-        boolean_constraint: bool = True,
     ):
         self.min = min
         self.max = max
-        self.boolean_constraint = boolean_constraint
 
     @override
     def __call__(self, observer_location, target_coord, obstime):
@@ -62,47 +57,10 @@ class AltitudeConstraint(Constraint):
         Returns
         -------
         `numpy.ndarray`
-            Boolean mask (if `boolean_constraint=True`) or scaled values (if `False`).
+            Boolean mask indicating whether the altitude constraint is met.
         """
         altaz = target_coord.transform_to(
             AltAz(obstime=obstime, location=observer_location)
         )
         alt = altaz.alt
-
-        if self.boolean_constraint:
-            return (self.min <= alt) & (alt <= self.max)
-        return self.max_best_rescale(alt, self.min, self.max, greater_than_max=0)
-
-    @staticmethod
-    def max_best_rescale(vals, min_val, max_val, greater_than_max=1):
-        """
-        Rescales an input array ``vals`` to be a score (between 0 and 1),
-        where ``max_val`` goes to 1, and ``min_val`` goes to 0.
-
-        Parameters
-        ----------
-        vals : array-like
-            The values that need to be rescaled.
-        min_val : float
-            Worst acceptable value (rescales to 0).
-        max_val : float
-            Best value cared about (rescales to 1).
-        greater_than_max : 0 or 1, optional
-            What is returned for ``vals`` above ``max_val``. Defaults to 1.
-
-        Returns
-        -------
-        numpy.ndarray
-            Array of floats between 0 and 1 inclusive.
-
-        Examples
-        --------
-        >>> import numpy as np
-        >>> from m4opt.constraints._altitude import AltitudeConstraint
-        >>> AltitudeConstraint.max_best_rescale(np.array([20, 30, 40, 45, 55, 70]), 35, 60)
-        array([0. , 0. , 0.2, 0.4, 0.8, 1. ])
-        """
-        rescaled = (vals - min_val) / (max_val - min_val)
-        rescaled[vals < min_val] = 0
-        rescaled[vals > max_val] = greater_than_max
-        return rescaled
+        return (self.min <= alt) & (alt <= self.max)
