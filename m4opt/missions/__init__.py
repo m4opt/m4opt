7b6eaef4ccba7e1a052ebdbc650d8ec8580a20a9
Leo Singer <leo.singer@ligo.org>
2025-03-11 14:28:21 -0400

    Add new mission: ZTF
    Fixes #30.

--- m4opt/missions/__init__.py
+++ m4opt/missions/__init__.py
@@ -3,5 +3,6 @@
 from ._core import Mission
 from ._ultrasat import ultrasat
 from ._uvex import uvex
+from ._ztf import ztf
 
-__all__ = ("Mission", "uvex", "ultrasat")
+__all__ = ("Mission", "uvex", "ultrasat", "ztf")
