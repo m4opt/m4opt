f8ddd33fbd505ea3cdb24374f9d5a2b3f867b44c
Leo Singer <leo.singer@ligo.org>
2025-05-11 21:48:09 -0400

    Make CLI tests a little more forgiving

--- m4opt/_cli/tests/test_core.py
+++ m4opt/_cli/tests/test_core.py
@@ -64,5 +64,4 @@ def test_mission(run_cli):
 
     result, value = run("--foo=bar")
     assert result.exit_code != 0
-    assert "Invalid value for '--foo'" in result.output
     assert "'bar' is not one of" in result.output
