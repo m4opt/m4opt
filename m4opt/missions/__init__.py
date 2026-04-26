d2fa369119491acdc5ef0c469fefb1046461d262
Leo Singer <leo.singer@ligo.org>
2026-04-25 21:56:15 -0400

    Add method to orient UVEX for a ground pass

--- m4opt/missions/__init__.py
+++ m4opt/missions/__init__.py
@@ -3,7 +3,7 @@
 from ._core import Mission
 from ._rubin import rubin
 from ._ultrasat import ultrasat
-from ._uvex import uvex
+from ._uvex import uvex, uvex_downlink_orientation
 from ._ztf import ztf
 
-__all__ = ("Mission", "rubin", "ultrasat", "uvex", "ztf")
+__all__ = ("Mission", "rubin", "ultrasat", "uvex", "uvex_downlink_orientation", "ztf")
--- m4opt/missions/_uvex.py
+++ m4opt/missions/_uvex.py
@@ -1,6 +1,7 @@
 import numpy as np
 from astropy import units as u
-from astropy.coordinates import SkyCoord
+from astropy.coordinates import Angle, SkyCoord, get_body
+from astropy.time import Time
 from regions import RectangleSkyRegion
 from synphot import Gaussian1D, SpectralElement
 
@@ -10,7 +11,7 @@ from ..constraints import (
     MoonSeparationConstraint,
     SunSeparationConstraint,
 )
-from ..dynamics import EigenAxisSlew
+from ..dynamics import EigenAxisSlew, nominal_roll
 from ..observer import TleObserverLocation
 from ..synphot import Detector
 from ..synphot.background import GalacticBackground, ZodiacalBackground
@@ -162,3 +163,206 @@ Examples
             ax.plot(wavelength, bandpass(wavelength), label=label)
         ax.legend()
 """
+
+
+def uvex_downlink_orientation(
+    time: Time,
+) -> tuple[SkyCoord, Angle]:
+    r"""
+    Get the target coordinates and roll for a UVEX downlink.
+
+    For UVEX, calculate the telescope boresight target coordinate and the roll
+    angle of the satellite for a ground contact. This is the orientation that
+    points the high gain antenna toward the Earth while maintaining pointing
+    constraints.
+
+    Parameters
+    ----------
+    time:
+        Time of the downlink.
+
+    Returns
+    -------
+    :
+        Target coordinate and roll angle.
+
+    Notes
+    -----
+    UVEX's high gain antenna is at a 45° angle to the spacecraft axes, and
+    points toward the cool side and opposite the telescope boresight. In terms
+    of the unit vector of the axes of the spacecraft coordinate system shown in
+    the illustration for the :meth:`~m4opt.dynamics.nominal_roll` method, the
+    direction :math:`\hat{\mathbf{a}}` of the antenna is:
+
+    .. math::
+        \hat{\mathbf{a}} = -\frac{\sqrt{2}}{2} \hat{\mathbf{x}} + \frac{\sqrt{2}}{2} \hat{\mathbf{z}}.
+
+    For a ground contact, the following conditions should be met, as shown in
+    the diagram below:
+
+    * Solar array drive axis is perpendicular to the direction of the Sun and
+      the Earth
+    * Earth is 135° from the telescope boresight
+    * Sun is ≥45° from the telescope boresight
+
+    The example below shows that all of these constraints are met for downlinks
+    at any time over the course of a year.
+
+    .. plot::
+        :include-source: False
+        :caption: Orientation of the UVEX spacecraft for a downlink.
+
+        from matplotlib import pyplot as plt
+        from matplotlib import patches
+        import numpy as np
+
+        fig = plt.figure(figsize=(4, 3))
+        ax = fig.add_subplot(aspect=1)
+        ax.set_xlim(-1, 1)
+        ax.set_ylim(-1, 1)
+        ax.xaxis.set_ticks([])
+        ax.yaxis.set_ticks([])
+        ax.set_frame_on(False)
+
+        # Origin
+        ax.plot(0, 0, ".", markersize=10, color="black")
+
+        # Antenna
+        ax.text(0.45, -0.05, "Antenna", ha="center", va="top")
+        ax.annotate(
+            "",
+            xy=(0, 0),
+            xytext=(0.9, 0),
+            xycoords="data",
+            textcoords="data",
+            arrowprops=dict(arrowstyle="<-"),
+        )
+
+        # Earth
+        ax.text(1, 0, "    Earth", va="center")
+        markersize = 16
+        for marker in "o+":
+            ax.plot(
+                1,
+                0,
+                marker=marker,
+                markerfacecolor="white",
+                color="black",
+                markeredgewidth=1,
+                markersize=markersize,
+                clip_on=False,
+            )
+
+        # Sun
+        # ax.text(-1, 0, "Sun   ", ha="right", va="center")
+        for sun_angle in np.arange(15, 180, 15):
+            color = str(sun_angle / 180)
+            angle = np.deg2rad(180 + sun_angle)
+            x = np.cos(angle)
+            y = np.sin(angle)
+            ax.plot(
+                x,
+                y,
+                marker="o",
+                markerfacecolor="white",
+                color=color,
+                markeredgewidth=1,
+                markersize=markersize,
+                clip_on=False,
+            )
+            ax.plot(
+                x,
+                y,
+                marker=".",
+                color=color,
+                markeredgewidth=1,
+                clip_on=False,
+            )
+
+        ax.add_patch(
+            patches.Arc((0, 0), width=0.5, height=0.5, theta1=135, theta2=180, linestyle="--")
+        )
+        angle = np.deg2rad((135 + 180) / 2)
+        ax.text(0.25 * np.cos(angle), 0.25 * np.sin(angle), "≥45°", ha="right", va="baseline")
+        ax.annotate(
+            "Sun  ",
+            xy=(0, 0),
+            xytext=(-0.9, 0),
+            xycoords="data",
+            textcoords="data",
+            ha="right",
+            va="center",
+            arrowprops=dict(arrowstyle="<-", linestyle="--"),
+        )
+
+        # Boresight
+        ax.annotate(
+            "Boresight",
+            xy=(0, 0),
+            xytext=(-np.sqrt(2) / 2, np.sqrt(2) / 2),
+            xycoords="data",
+            textcoords="data",
+            arrowprops=dict(arrowstyle="<-"),
+            ha="right",
+            va="bottom",
+            rotation=-45,
+            clip_on=False,
+        )
+
+        # Mark angle
+        ax.add_patch(patches.Arc((0, 0), width=0.5, height=0.5, theta1=0, theta2=135))
+        angle = np.deg2rad(135 / 2)
+        ax.text(0.25 * np.cos(angle), 0.25 * np.sin(angle), "135°", ha="left", va="bottom")
+
+    .. plot::
+        :caption: Satisfaction of downlink constraints as a function of time.
+
+        import numpy as np
+        from astropy import units as u
+        from astropy.coordinates import (
+            SkyCoord,
+            CartesianRepresentation,
+            get_body,
+        )
+        from astropy.time import Time
+        from m4opt.missions import uvex as mission, uvex_downlink_orientation
+        from matplotlib import pyplot as plt
+
+        time = Time("2025-01-01") + np.linspace(0, 1, 1000) * u.year
+        target, roll = uvex_downlink_orientation(time)
+
+        observer_location = mission.observer_location(time)
+        sun = get_body("sun", time, observer_location)
+        earth = get_body("earth", time, observer_location)
+        spacecraft_frame = target.skyoffset_frame(roll)
+        antenna = SkyCoord(
+            CartesianRepresentation(-1 / np.sqrt(2), 0, 1 / np.sqrt(2)), frame=spacecraft_frame
+        )
+        solar_array = SkyCoord(CartesianRepresentation(0, 1, 0), frame=spacecraft_frame)
+
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        ax.yaxis.set_major_locator(plt.MultipleLocator(45))
+        ax.yaxis.set_major_formatter(plt.FormatStrFormatter("%g°"))
+        dt = time.datetime
+        ax.plot(dt, target.separation(earth), label=r"Target $\leftrightarrow$ Earth")
+        ax.plot(dt, target.separation(sun), label=r"Target $\leftrightarrow$ Sun")
+        ax.plot(dt, solar_array.separation(sun), label=r"Solar axis $\leftrightarrow$ Sun")
+        ax.plot(dt, antenna.separation(earth), label=r"Target $\leftrightarrow$ antenna")
+        ax.legend(title="Separation", loc="center right", bbox_to_anchor=(0.975, 0.25))
+    """
+    observer_location = uvex.observer_location(time)
+    sun_coord = get_body("sun", time, observer_location)
+    earth_coord = get_body("earth", time, observer_location)
+    roll = nominal_roll(observer_location, earth_coord, time)
+    offset_frame = earth_coord.skyoffset_frame(roll)
+    target_coord = SkyCoord(180 * u.deg, -45 * u.deg, frame=offset_frame)
+    violates_sun_constraint = target_coord.separation(sun_coord) <= 45 * u.deg
+    target_coord = SkyCoord(
+        180 * u.deg,
+        np.where(violates_sun_constraint, 45, -45) * u.deg,
+        frame=offset_frame,
+    ).transform_to(earth_coord.frame)
+    roll = nominal_roll(observer_location, target_coord, time)
+    roll[~violates_sun_constraint] += 180 * u.deg
+    return target_coord, Angle(roll).wrap_at(180 * u.deg)
