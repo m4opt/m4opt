69e9101fd2449dc875e1d130f9f057e7ec803b95
Athena Devashish <athenadevashish@gmail.com>
2026-07-30 12:25:32 -0400

    Update schedule.py
    Make readable for ground-based missions

--- m4opt/_cli/schedule.py
+++ m4opt/_cli/schedule.py
@@ -6,7 +6,7 @@ import numpy as np
 import synphot
 import typer
 from astropy import units as u
-from astropy.coordinates import ICRS, Distance, SkyCoord
+from astropy.coordinates import ICRS, Distance, SkyCoord, EarthLocation
 from astropy.table import QTable, vstack
 from astropy.time import Time
 from astropy_healpix import HEALPix
@@ -696,7 +696,9 @@ def schedule(
                 name="observer_location",
             )
             table["observer_location"].info.description = "Position of the spacecraft"
-
+            if isinstance(mission.observer_location, EarthFixedObserverLocation):
+                table["observer_location"].__class__ = EarthLocation
+                
             # Add slew segments to table.
             if len(table) > 0:
                 nrows = len(table) - 1
