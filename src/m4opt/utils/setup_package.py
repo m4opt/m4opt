01f3936971c8d4557ba53d789d8842066dae0d64
Leo Singer <leo.singer@ligo.org>
2026-08-09 23:40:13 -0400

    Fix limited API settings

--- src/m4opt/utils/setup_package.py
+++ src/m4opt/utils/setup_package.py
@@ -9,9 +9,11 @@ def get_extensions():
             "m4opt.utils._numpy",
             ["src/m4opt/utils/_numpy.c"],
             define_macros=[
+                ("Py_LIMITED_API", 0x030B0000),
                 ("NPY_TARGET_VERSION", "NPY_2_0_API_VERSION"),
                 ("NPY_NO_DEPRECATED_API", "NPY_2_0_API_VERSION"),
             ],
             include_dirs=[np.get_include()],
+            py_limited_api=True,
         )
     ]
