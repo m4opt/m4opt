35a1ace4e8e01d6da5e65f6401dd6c0e99a23e08
Leo Singer <leo.singer@ligo.org>
2026-08-12 16:08:22 -0400

    Add benchmark for count_intersect1d

--- src/m4opt/utils/tests/test_numpy.py
+++ src/m4opt/utils/tests/test_numpy.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 from hypothesis import given
 from hypothesis.extra import numpy as xp
 
@@ -18,3 +19,18 @@ def count_intersect1d_slow(a, b):
 @given(a=sets1d, b=sets1d)
 def test_count_intersect1d(a, b):
     assert count_intersect1d(a, b) == count_intersect1d_slow(a, b)
+
+
+def setup_set1d():
+    return np.sort(np.random.choice(1_000_000, 100_000, replace=False)).astype(
+        np.intp, copy=False
+    )
+
+
+def setup():
+    return (setup_set1d(), setup_set1d()), {}
+
+
+@pytest.mark.parametrize("implementation", (count_intersect1d, count_intersect1d_slow))
+def test_benchmark(implementation, benchmark):
+    benchmark.pedantic(implementation, setup=setup, rounds=100, warmup_rounds=1)
