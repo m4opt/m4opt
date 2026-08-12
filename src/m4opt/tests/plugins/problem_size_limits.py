24f415438cf95874f9cc88db238b9ed06c1d890a
Leo Singer <leo.singer@ligo.org>
2026-08-12 15:26:03 -0400

    Fix incorrectly running every test twice
    This fix was identified using gpt-5.3-codex via Continue.

--- src/m4opt/tests/plugins/problem_size_limits.py
+++ src/m4opt/tests/plugins/problem_size_limits.py
@@ -2,9 +2,11 @@ import pytest
 from docplex.mp.utils import DOcplexLimitsExceeded
 
 
+@pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_call(item):
     """Skip tests that exceed the Gurobi or CPLEX problem size."""
-    try:
-        item.runtest()
-    except DOcplexLimitsExceeded:
+    outcome = yield
+    if outcome.excinfo is not None and issubclass(
+        outcome.excinfo[0], DOcplexLimitsExceeded
+    ):
         pytest.skip("requires full version of CPLEX")
