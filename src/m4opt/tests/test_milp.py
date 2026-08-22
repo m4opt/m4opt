a7089f8347556058da9b97276d970b067b662fab
Leo Singer <leo.singer@ligo.org>
2026-08-22 10:23:38 -0400

    Refactor duplicated code into fixtures

--- src/m4opt/tests/test_milp.py
+++ src/m4opt/tests/test_milp.py
@@ -2,7 +2,6 @@
 
 from tempfile import gettempdir
 
-import docplex.mp.model
 import numpy as np
 import pytest
 from astropy import units as u
@@ -14,75 +13,78 @@ problem_size_limits = pytest.mark.parametrize(
 )
 
 
+@pytest.fixture
+def m():
+    with Model() as m:
+        yield m
+
+
+@pytest.fixture(params=_VARIABLE_TYPES)
+def add_vars(m, request):
+    return getattr(m, f"{request.param}_vars")
+
+
 @problem_size_limits
-def test_cplex(num_vars):
+def test_cplex(m, num_vars):
     """Test that CPLEX solver works with small and big problems."""
-    m = docplex.mp.model.Model()
     m.binary_var_list(num_vars)
     m.solve()
 
 
 def test_cplex_parameters():
     """Test configuration of CPLEX solver parameters."""
-    m = Model()
-    assert m.context.cplex_parameters.mip.pool.capacity.value == 0
-    assert m.context.cplex_parameters.mip.strategy.file.value == 1
-    assert m.context.cplex_parameters.parallel.value == -1
-    assert m.context.cplex_parameters.threads.value == 0
-    assert m.context.cplex_parameters.timelimit.value == 1e75
-    assert m.context.cplex_parameters.workmem.value == 2048
-    assert m.context.cplex_parameters.workdir.value == gettempdir()
-    assert m.context.solver.log_output
+    with Model() as m:
+        assert m.context.cplex_parameters.mip.pool.capacity.value == 0
+        assert m.context.cplex_parameters.mip.strategy.file.value == 1
+        assert m.context.cplex_parameters.parallel.value == -1
+        assert m.context.cplex_parameters.threads.value == 0
+        assert m.context.cplex_parameters.timelimit.value == 1e75
+        assert m.context.cplex_parameters.workmem.value == 2048
+        assert m.context.cplex_parameters.workdir.value == gettempdir()
+        assert m.context.solver.log_output
 
-    m = Model(timelimit=1 * u.minute, jobs=3)
-    assert m.context.cplex_parameters.mip.pool.capacity.value == 0
-    assert m.context.cplex_parameters.mip.strategy.file.value == 1
-    assert m.context.cplex_parameters.parallel.value == -1
-    assert m.context.cplex_parameters.threads.value == 3
-    assert m.context.cplex_parameters.timelimit.value == 60
-    assert m.context.cplex_parameters.workmem.value == 2048
-    assert m.context.cplex_parameters.workdir.value == gettempdir()
-    assert m.context.solver.log_output
+    with Model(timelimit=1 * u.minute, jobs=3) as m:
+        assert m.context.cplex_parameters.mip.pool.capacity.value == 0
+        assert m.context.cplex_parameters.mip.strategy.file.value == 1
+        assert m.context.cplex_parameters.parallel.value == -1
+        assert m.context.cplex_parameters.threads.value == 3
+        assert m.context.cplex_parameters.timelimit.value == 60
+        assert m.context.cplex_parameters.workmem.value == 2048
+        assert m.context.cplex_parameters.workdir.value == gettempdir()
+        assert m.context.solver.log_output
 
-    m = Model(timelimit=1 * u.minute, jobs=3, memory=5 * u.GiB)
-    assert m.context.cplex_parameters.mip.pool.capacity.value == 0
-    assert m.context.cplex_parameters.mip.strategy.file.value == 3
-    assert m.context.cplex_parameters.parallel.value == -1
-    assert m.context.cplex_parameters.threads.value == 3
-    assert m.context.cplex_parameters.timelimit.value == 60
-    assert m.context.cplex_parameters.workmem.value == 5120
-    assert m.context.cplex_parameters.workdir.value == gettempdir()
-    assert m.context.solver.log_output
+    with Model(timelimit=1 * u.minute, jobs=3, memory=5 * u.GiB) as m:
+        assert m.context.cplex_parameters.mip.pool.capacity.value == 0
+        assert m.context.cplex_parameters.mip.strategy.file.value == 3
+        assert m.context.cplex_parameters.parallel.value == -1
+        assert m.context.cplex_parameters.threads.value == 3
+        assert m.context.cplex_parameters.timelimit.value == 60
+        assert m.context.cplex_parameters.workmem.value == 5120
+        assert m.context.cplex_parameters.workdir.value == gettempdir()
+        assert m.context.solver.log_output
 
 
-def test_best_bound():
-    m = Model()
+def test_best_bound(m):
     x = m.binary_var()
     m.maximize(x)
     m.solve()
     assert m.best_bound == m.solve_details.best_bound == m.objective_value == 1
 
 
-@pytest.mark.parametrize("tp", _VARIABLE_TYPES)
-def test_cplex_add_var_array(tp):
+def test_cplex_add_var_array(m, add_vars):
     """Test convenience functions for adding arrays of decision variables."""
-    m = Model()
-
-    result = getattr(m, f"{tp}_vars")()
+    result = add_vars()
     assert not isinstance(result, np.ndarray)
 
-    result = getattr(m, f"{tp}_vars")((6, 4))
+    result = add_vars((6, 4))
     assert result.shape == (6, 4)
 
-    result = getattr(m, f"{tp}_vars")(
-        (6, 4), lb=np.full((6, 4), 0.5), ub=np.full((6, 4), 1)
-    )
+    result = add_vars((6, 4), lb=np.full((6, 4), 0.5), ub=np.full((6, 4), 1))
     assert result.shape == (6, 4)
 
-    assert getattr(m, f"number_of_{tp}_variables") == 49
+    assert m.number_of_variables == 49
 
 
-@pytest.mark.parametrize("tp", _VARIABLE_TYPES)
 @pytest.mark.parametrize(
     "rhs_shape",
     ((), 2, (3, 2)),
@@ -100,10 +102,8 @@ def test_cplex_add_var_array(tp):
         "m.max(*x.ravel()) <= 0",
     ),
 )
-def test_cplex_operators(tp, rhs_shape, expr):
+def test_cplex_operators(m, add_vars, rhs_shape, expr):
     """Test adding constraints by broadcasting variables."""
-    m = Model()
-    add_vars = getattr(m, f"{tp}_vars")
     constraint = eval(
         expr, None, {"m": m, "x": add_vars((3, 2)), "y": add_vars(rhs_shape)}
     )
@@ -111,15 +111,12 @@ def test_cplex_operators(tp, rhs_shape, expr):
     m.add_constraints_(constraint)
 
 
-@pytest.mark.parametrize("tp", _VARIABLE_TYPES)
 @pytest.mark.parametrize(
     "rhs_shape",
     ((), 2, (3, 2)),
 )
-def test_cplex_broadcast_indicator(tp, rhs_shape):
+def test_cplex_broadcast_indicator(m, add_vars, rhs_shape):
     """Test adding indicator constraints by broadcasting variables."""
-    m = Model()
-    add_vars = getattr(m, f"{tp}_vars")
     x = m.binary_vars((3, 2))
     y = add_vars(rhs_shape)
     constraint = (x == 1) >> (y >= 0)
@@ -131,8 +128,7 @@ def test_cplex_broadcast_indicator(tp, rhs_shape):
 @pytest.mark.parametrize(
     "suffix", [".lp", ".mps", ".sav", ".lp.gz", ".mps.gz", ".sav.gz"]
 )
-def test_to_stream(suffix, tmp_path):
-    m = Model()
+def test_to_stream(m, suffix, tmp_path):
     x = m.binary_var()
     m.maximize(x)
     with (tmp_path / "model").with_suffix(suffix).open("wb") as f:
