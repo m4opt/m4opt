342e7190e7c0d7eb05e2f7406166c339eb764a72
Leo Singer <leo.singer@ligo.org>
2026-08-27 08:16:56 -0400

    Add apply method

--- src/m4opt/utils/functional.py
+++ src/m4opt/utils/functional.py
@@ -5,12 +5,31 @@ from itertools import groupby
 from operator import itemgetter
 from typing import TypeVar
 
-__all__ = ("groupby_unsorted",)
+__all__ = ("apply", "groupby_unsorted")
 
 Item = TypeVar("Item")
 Key = TypeVar("Key")
 first = itemgetter(0)
 second = itemgetter(1)
+
+
+def apply[*Ts, R](func: Callable[[*Ts], R], args: tuple[*Ts]) -> R:
+    """Invoke a function, unpacking arguments.
+
+    The function is called such that ``apply(foo, (bar, bat))`` is equivalent
+    to ``foo(bar, bat)``.
+
+    >>> from m4opt.utils.functional import apply
+    >>> def add(a, b):
+    ...     return a + b
+    ...
+    >>> args = (1, 2)
+    >>> apply(add, args)
+    3
+    >>> add(*args)
+    3
+    """
+    return func(*args)
 
 
 def groupby_unsorted[Item, Key](
